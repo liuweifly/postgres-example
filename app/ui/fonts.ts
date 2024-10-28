06857fda5be3a278731e8076b9f9e8e839de3f8f
刘苇 <liuweifly@yahoo.com>
2024-10-28 17:54:56 +0800

    前11章修改内容

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,11 +1,12 @@
-import { Inter, Lusitana, Playwrite_GB_S } from 'next/font/google';
+import { Inter, Lusitana, Roboto } from 'next/font/google';
  
 export const inter = Inter({ subsets: ['latin'] });
 export const lusitana = Lusitana({
   weight: ['400', '700'],
   subsets: ['latin'],
 });
-export const playwrite = Playwrite_GB_S({
-  weight: ['400'],
-//   subsets: ['latin'],
+export const playwrite = Roboto({
+  weight: '400',
+//   display: 'swap',
+  //subsets: ['latin'],
 });
